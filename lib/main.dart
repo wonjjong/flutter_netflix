ad68a1879a92e8395f7a6629185d45dc9265133a
wonjjong <wonjjong.dev@gmail.com>
2025-08-18 01:04:39 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // TabController tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'netflix demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.dark(secondary: Colors.white),
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Center(child: Text('home')),
+              Center(child: Text('search')),
+              Center(child: Text('save')),
+              Center(child: Text('more')),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
